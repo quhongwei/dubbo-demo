418d41bc4319944771a540e3819b51e24b823c0b
hongwei.quhw <hongwei.quhw@antfin.com>
2020-09-14 13:26:49 +0800

    测试demo

---
+++ consumer-2-7-9/src/main/java/com/dubbo/example/dubbodemo/impl/HelloInvoke.java
@@ -0,0 +1,51 @@
+package com.dubbo.example.dubbodemo.impl;
+
+import com.dubbo.example.dubbodemo.api.HelloService;
+import com.dubbo.example.dubbodemo.util.NetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * @author: hongwei.quhw
+ * @date: 2020-09-12 22:24
+ */
+
+public class HelloInvoke implements InitializingBean {
+    private static final Logger LOGGER                  = LoggerFactory.getLogger(HelloInvoke.class);
+
+    private HelloService helloService;
+
+    @Override
+    public void afterPropertiesSet() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while(true) {
+                    try {
+                        String ret = helloService.sayHello("consumer-2-7-9 host=" + NetUtil.getLocalAddress().toString());
+                        System.out.println(ret);
+                        LOGGER.info(ret);
+
+                    } catch (Exception e) {
+                        LOGGER.error("", e);
+                    }
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+    }
+
+
+    public void setHelloService(HelloService helloService) {
+        this.helloService = helloService;
+    }
+}
