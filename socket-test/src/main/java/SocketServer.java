89004c5a9728c2584a7c1918ecab3fba60b8ad82
hongwei.quhw <hongwei.quhw@antfin.com>
2020-11-03 10:05:21 +0800

    socket test

---
+++ socket-test/src/main/java/SocketServer.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+
+/**
+ * Copyright (C) 2013-2018 Ant Financial Services Group
+ *
+ * @author quhongwei
+ * @version : SocketServer.java, v 0.1 2020年11月03日 10:05 quhongwei Exp $
+ */
+public class SocketServer {
+}
